327e76511bc2df33fa4b428c984a745a8d0b7f54
Hsuching <martin.hsuching@gmail.com>
2013-02-21 18:07:20 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-$(function(){
-	
-	var imp = impress();
-	
-	$('#arrowLeft').click(function(e){
-		imp.prev();
-		e.preventDefault();
-	});
-	
-	$('#arrowRight').click(function(e){
-		imp.next();
-		e.preventDefault();
-	});
+//$(function(){
+//
+//	var imp = impress();
+//
+//	$('#arrowLeft').click(function(e){
+//		imp.prev();
+//		e.preventDefault();
+//	});
+//
+//	$('#arrowRight').click(function(e){
+//		imp.next();
+//		e.preventDefault();
+//	});
+//
+//});
 
+
+$.ajax({
+    url:'../DKOM2013/json/demo.json',
+    type:'post',
+    dataType:'json',
+    success:function(){
+        alert("success");
+    },
+    error:function(){
+        alert("error");
+    }
+})
+
+
+$('#loading').ajaxSend(function(){
+   alert("Hello")
 });
